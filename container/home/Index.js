620269c6526e554f9098d4071cca981256201781
武扬 <956826374@qq.com>
2018-11-14 14:15:14 +0800

    完成自动监听文件变化，并且自动打包

--- container/home/Index.js
+++ container/home/Index.js
@@ -8,7 +8,10 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>Holle world</div>
+        <>
+            <div>来自服务端的内容提示：</div>
+            <h1>渣比群主!</h1>
+        </>
     )
 }
 
